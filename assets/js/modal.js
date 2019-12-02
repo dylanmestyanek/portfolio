5443beba0a124bf3c66da06e3d10fc31c5fe16fd
Dylan Mestyanek <mestyanekdylan@gmail.com>
2019-12-02 17:50:47 -0600

    Fixed click event mishappenings when attempting to view project cards. Entire project card is now clickable in order to view modal conataining project details. Not only just the picture.

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,14 +1,40 @@
 // Get the modal
 var modals = document.querySelectorAll("#modal");
 
-// Get the button that opens the modal
+// Get all project images 
 var projectImages = document.querySelectorAll("#project-image");
+
+// Get all project titles 
+var projectTitles = document.querySelectorAll("#project-title");
+
+// Get all project title text
+var projectTitlesText = document.querySelectorAll("#project-title h3");
 
 // Get the <span> element that closes the modal
 var closeButtons = document.querySelectorAll(".close");
 
-// When the user clicks on the button, open the modal
+// When the user clicks on the image, open the modal
 projectImages.forEach(btn => btn.addEventListener('click', e => {
+    const index = e.target.dataset.image;
+
+    modals[index].style.display = "block";
+
+    document.querySelector("#main").style.filter = "blur(2px)";
+}))
+
+// When the user clicks on the title, open the modal
+projectTitles.forEach(title => title.addEventListener('click', e => {
+
+    const index = e.target.dataset.image;
+
+    modals[index].style.display = "block";
+
+    document.querySelector("#main").style.filter = "blur(2px)";
+}))
+
+// When the user clicks on the title, open the modal
+projectTitlesText.forEach(title => title.addEventListener('click', e => {
+
     const index = e.target.dataset.image;
 
     modals[index].style.display = "block";
@@ -19,7 +45,7 @@ projectImages.forEach(btn => btn.addEventListener('click', e => {
 // When the user clicks on <span> (x), close the modal
 closeButtons.forEach(button => button.addEventListener('click', e => {
   const index = +e.target.dataset.span;
-  console.log(e.target.dataset.span)
+
   modals[index].style.display = "none";
   document.querySelector("#main").style.filter = "blur(0px)";
 }))
